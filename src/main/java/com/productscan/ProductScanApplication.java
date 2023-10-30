8b54400bc9c4075322a36ec64bc8b41ac0315345
Tim01Bro <arthaos14@gmail.com>
2023-10-30 23:28:40 +0300

    Init commit

---
+++ src/main/java/com/productscan/ProductScanApplication.java
@@ -0,0 +1,13 @@
+package com.productscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductScanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/productscan/ProductScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
